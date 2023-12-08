ecb6f40815b537c41cec09cf22a15873484e1b0b
hectorruiz1 <hector.ruiz@kubide.es>
2023-12-08 15:35:47 +0100

    fix(favicon): changed favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import webmanifest from 'astro-webmanifest';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(), 
+    react(),
+  ]
 });
